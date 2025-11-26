a4dfa59cb144b050cb755e9978a52819eb25fc5d
TMN-INVESTMENT <tanzaniamininginvestment@gmail.com>
2025-11-26 20:21:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4807,7 +4807,7 @@ function loadEnhancedReferrals() {
     // Update referral code display
     document.getElementById('user-referral-code-display').textContent = db.currentUser.referral_code;
     document.getElementById('referral-link-text').textContent = 
-        `https://tanzania-mining.com/register?ref=${db.currentUser.referral_code}`;
+        `tmn-investment-mn6p.vercel.app/register?ref=${db.currentUser.referral_code}`;
     
     // Populate referrals table
     populateReferralsTable(userReferrals);
